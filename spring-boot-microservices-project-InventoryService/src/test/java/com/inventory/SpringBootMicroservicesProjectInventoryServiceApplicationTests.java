aaccad58ea25b199204bf77d45530bcf8c30f2a8
Dev2 <Dev2@gmail.com>
2023-05-18 17:33:26 +0530

    BootProjwithMicroservices

---
+++ spring-boot-microservices-project-InventoryService/src/test/java/com/inventory/SpringBootMicroservicesProjectInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroservicesProjectInventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
